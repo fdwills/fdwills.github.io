ff2711aedf1bf4968c08e01c0afe472916af708d
wei.renzhong <renzhong.wei@dena.com>
2014-03-05 14:42:25 +0900

    .gitignore is now working

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'capistrano', '~> 3.0.1'
+gem 'hookspec', git: 'https://github.com/fdwills/hookspec'
---
+++ git_hooks/post-update.sh
@@ -0,0 +1,1 @@
+/Users/wei.renzhong/my-git/fdwills.github.io/vendor/bundle/ruby/2.0.0/bundler/gems/hookspec-de7aeb27a97a/lib/hookspec/templates/post-update.sh
